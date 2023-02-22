dce448141b837048e923db6d79fb14219dfcedfb
hidaka-fujimoto1609 <hidaka1674@gmail.com>
2023-02-22 22:12:26 +0900

    ログイン機能実装

---
+++ app/database/migrations/2023_02_20_085833_create_personals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('health_check_date');
+            $table->string('department_name','100');
+            $table->date('birth');
+            $table->text('contents');
+            $table->string('image_path','200')->nullable();
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personas');
+    }
+}
---
+++ app/database/migrations/2023_02_20_134457_create_joins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJoinsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('joins', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('site_id');
+            $table->tinyInteger('join_flg')->default(0);
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('joins');
+    }
+}
